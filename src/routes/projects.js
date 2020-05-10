42280d824726bee42fd7027e9dd0e4265f595310
ahmedalwardani <aalwa049@uottawa.ca>
2020-05-10 17:02:31 -0400

    add configurations and projects routes

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -10,18 +10,18 @@ module.exports = db => {
   router.delete("/projects", (request, response) => {
     console.log(request.body);
     db.query(("DELETE FROM projects WHERE id=$1 AND user_id=$2"), [request.body.projectId, request.body.userId]).then((resp) => {
-      if(resp.rowCount === 0) {
+      if (resp.rowCount === 0) {
         // ? Simulate delay
         setTimeout(() => {
           response.status(400).json({});
-        }, 2000)
+        }, 2000);
       } else {
         // ? Simulate delay
         setTimeout(() => {
           response.status(200).json({});
-        }, 2000)
+        }, 2000);
       }
-    })
+    });
   });
 
   return router;
