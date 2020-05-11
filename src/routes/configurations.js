d055a3f110881ded66a980846b80e07a87ae0a07
ahmedalwardani <aalwa049@uottawa.ca>
2020-05-10 22:06:48 -0400

    add screenshots to projects

--- src/routes/configurations.js
+++ src/routes/configurations.js
@@ -1,25 +1,41 @@
 const router = require("express").Router();
 
 module.exports = db => {
-  //Get all configureations
+  // Get all configureations
   router.get("/configurations", (request, response) => {
-    db.query(("SELECT * FROM configurations")).then(data => {
+    db.query("SELECT * FROM configurations WHERE user_id=$1", [
+      request.query.userId
+    ]).then(data => {
       response.json(data.rows);
     });
   });
 
-  //Create a new configuration
+  // Create a new configuration
   router.post("/configurations", (request, response) => {
-    db.query(("INSERT INTO configurations VALUES ($1, $2, $3)"), [request.body.configuration.name, request.body.configuration.projectId, request.body.configuration.data]).then((resp) => {
-      setTimeout(() => {
-        response.status(400).json({});
-      }, 2000);
-    }).then(err => console.log(err));
+    db.query("INSERT INTO configurations VALUES ($1, $2, $3)", [
+      request.body.configuration.name,
+      request.body.configuration.projectId,
+      request.body.configuration.data
+    ])
+      .then(resp => {
+        setTimeout(() => {
+          response.status(400).json({});
+        }, 2000);
+      })
+      .then(err => console.log(err));
   });
 
-  //Update an existing configuration
+  // Update an existing configuration
   router.put("/configurations", (request, response) => {
-    db.query(("UPDATE configurations SET name=$1, config_data=$2 FROM projects WHERE configurations.id=$3 and projects.user_id=$4"), [request.body.configuration.name, request.body.configuration.data, request.body.configuration.id, request.body.userId]).then((resp) => {
+    db.query(
+      "UPDATE configurations SET name=$1, config_data=$2 FROM projects WHERE configurations.id=$3 and projects.user_id=$4",
+      [
+        request.body.configuration.name,
+        request.body.configuration.data,
+        request.body.configuration.id,
+        request.body.userId
+      ]
+    ).then(resp => {
       if (resp.rowCount === 0) {
         setTimeout(() => {
           response.status(400).json({});
@@ -32,9 +48,12 @@ module.exports = db => {
     });
   });
 
-  //Delete a configuration
+  // Delete a configuration
   router.delete("/configurations", (request, response) => {
-    db.query(("DELETE FROM configurations USING projects WHERE configurations.id=$1 AND projects.user_id=$2"), [request.body.configurationId, request.body.userId]).then((resp) => {
+    db.query(
+      "DELETE FROM configurations USING projects WHERE configurations.id=$1 AND projects.user_id=$2",
+      [request.body.configurationId, request.body.userId]
+    ).then(resp => {
       if (resp.rowCount === 0) {
         // ? Simulate delay
         setTimeout(() => {
@@ -50,9 +69,4 @@ module.exports = db => {
   });
 
   return router;
-  
 };
-
-
-
-
