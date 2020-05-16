cf59806ebb8d9ada46e24ede9063e5886da5b790
aliyasser20 <aliyasser20@hotmail.com>
2020-05-16 02:43:24 -0400

    commented all schema seeds

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -1,73 +1,73 @@
--- TRUNCATE projects, configurations;
+-- -- TRUNCATE projects, configurations;
 
-INSERT INTO
-  projects
-  (name, description, user_id, model_link)
-VALUES
-  (
-    'shaver',
-    'philips shaver very good',
-    'facebook|10158865693899363',
-    'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'
-  ),
+-- INSERT INTO
+--   projects
+--   (name, description, user_id, model_link)
+-- VALUES
+--   (
+--     'shaver',
+--     'philips shaver very good',
+--     'facebook|10158865693899363',
+--     'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'
+--   ),
 
-  ('mouse', 'model of computer mouse', 'facebook|10158865693899363', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
-
-
-  ('tea', 'model of a cup of team', 'facebook|10158865693899363', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
-
-  (
-    'shaver',
-    'philips shaver very good',
-    'google-oauth2|117948270148318970184',
-    'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'
-  ),
-
-  ('mouse', 'model of computer mouse', 'google-oauth2|117948270148318970184
-', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
-
-  ('tea', 'model of a cup of team', 'google-oauth2|117948270148318970184
-', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
-
-  (
-    'shaver',
-    'philips shaver very good',
-    'auth0|5eb6f6ab1cc1ac0c14984e4e',
-    'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'
-  ),
-
-  ('mouse', 'model of computer mouse', 'auth0|5eb6f6ab1cc1ac0c14984e4e', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
-
-  ('tea', 'model of a cup of team', 'auth0|5eb6f6ab1cc1ac0c14984e4e', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb');
+--   ('mouse', 'model of computer mouse', 'facebook|10158865693899363', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
 
 
-INSERT INTO configurations
-  (name, project_id, config_data)
-VALUES
-  (
-    'rocket-config-1',
-    1,
-    'rocket config 1 data'
-),
+--   ('tea', 'model of a cup of team', 'facebook|10158865693899363', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
 
-  (
-    'mustard-config-1',
-    2,
-    'mustard config 1 data'
-),
-  (
-    'duck-config-1',
-    3,
-    'duck config 1 data'
-);
+--   (
+--     'shaver',
+--     'philips shaver very good',
+--     'google-oauth2|117948270148318970184',
+--     'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'
+--   ),
 
-INSERT INTO themes
-  (user_id, theme)
-VALUES
-  ('facebook|10158865693899363', 'electric violet'),
-  ('google-oauth2|117948270148318970184', 'sea weed'),
-  ('auth0|5eb6f6ab1cc1ac0c14984e4e', 'dimigo'),
-  ('auth0|5eb850cb1cc1ac0c149bee47','cherry')
+--   ('mouse', 'model of computer mouse', 'google-oauth2|117948270148318970184
+-- ', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
+
+--   ('tea', 'model of a cup of team', 'google-oauth2|117948270148318970184
+-- ', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
+
+--   (
+--     'shaver',
+--     'philips shaver very good',
+--     'auth0|5eb6f6ab1cc1ac0c14984e4e',
+--     'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'
+--   ),
+
+--   ('mouse', 'model of computer mouse', 'auth0|5eb6f6ab1cc1ac0c14984e4e', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb'),
+
+--   ('tea', 'model of a cup of team', 'auth0|5eb6f6ab1cc1ac0c14984e4e', 'https://res.cloudinary.com/aajfinal/raw/upload/v1589055152/models/duck_sqoq6h.glb');
+
+
+-- INSERT INTO configurations
+--   (name, project_id, config_data)
+-- VALUES
+--   (
+--     'rocket-config-1',
+--     1,
+--     'rocket config 1 data'
+-- ),
+
+--   (
+--     'mustard-config-1',
+--     2,
+--     'mustard config 1 data'
+-- ),
+--   (
+--     'duck-config-1',
+--     3,
+--     'duck config 1 data'
+-- );
+
+-- INSERT INTO themes
+--   (user_id, theme)
+-- VALUES
+--   ('facebook|10158865693899363', 'electric violet'),
+--   ('google-oauth2|117948270148318970184', 'sea weed'),
+--   ('auth0|5eb6f6ab1cc1ac0c14984e4e', 'dimigo'),
+--   ('auth0|5eb850cb1cc1ac0c149bee47','cherry')
 
 
 
